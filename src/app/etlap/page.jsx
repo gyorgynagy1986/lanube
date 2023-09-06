d1113e8777fdfec6d0de6d8bd5df453d231752e2
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-09-06 12:14:10 +0200

    optimalization

--- src/app/etlap/page.jsx
+++ src/app/etlap/page.jsx
@@ -2,25 +2,26 @@
 
 import Image from "next/image";
 import etlap from "../../../public/assets/menu/1.webp";
-import itallap from "../../../public/assets/menu/2.webp";
+import itallap  from "../../../public/assets/menu/2.webp";
 import style from "./page.module.css";
-
 
 const Menu = () => {
   const handleClickFood = () => {
-    window.location.href = "/etlap.pdf";
+    window.open("/etlap.pdf", "_blank")
   };
 
   const handleClickDrinks = () => {
-    window.location.href = "/itallap.pdf";
+    window.open("/itallap.pdf", "_blank");
   };
 
   return (
     <>
       <h1 className={style.h1}>Étlap</h1>
       <div className={style.container}>
+
         <Image
           style={{ cursor: "pointer" }}
+          className={style.imageHoverZoom}
           onClick={handleClickFood}
           placeholder="blur"
           priority
@@ -28,6 +29,7 @@ const Menu = () => {
           src={itallap}
         />
         <Image
+        className={style.imageHoverZoom}
           style={{ cursor: "pointer" }}
           onClick={handleClickDrinks}
           placeholder="blur"
