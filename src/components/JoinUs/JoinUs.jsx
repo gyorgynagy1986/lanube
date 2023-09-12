66793ca41c182817904afb30753af7da415532ac
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-09-12 14:44:12 +0200

    text-gallery

--- src/components/JoinUs/JoinUs.jsx
+++ src/components/JoinUs/JoinUs.jsx
@@ -8,15 +8,17 @@ import { getJoinUsContent } from "@/data/language-handler";
 
 const text = (
   <p className={style.h2}>
-   Ha Te is szívesen csatlakoznál csapatunkhoz, küldd el önéletrajzod az {" "}
-    <span className="emailSpan">hola@lanubebp.hu</span> e-mail címre, és hamarosan felvesszük Veled a kapcsolatot!
+    Ha Te is szívesen csatlakoznál csapatunkhoz, küldd el önéletrajzod az{" "}
+    <span className={style.email}>hola@lanubebp.hu</span> e-mail címre, és
+    hamarosan felvesszük Veled a kapcsolatot!
   </p>
 );
 
 const textEn = (
   <p className={style.h2}>
-    If you would also like to join our team, send your CV to {" "}
-    <span className="emailSpan">hola@lanubebp.hu</span> and we will contact you soon!
+    If you would also like to join our team, send your CV to{" "}
+    <span className={style.email}>hola@lanubebp.hu</span> and we will contact
+    you soon!
   </p>
 );
 
